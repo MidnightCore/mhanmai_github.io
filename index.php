312741fb609d72f4d496412d243171787b3981e7
MidnightCore <setsuna8741@gmail.com>
2021-08-13 12:36:40 +0700

    update 1
    สร้าง template เว็บไซต์

---
+++ index.php
@@ -0,0 +1,416 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- set the encoding of your site -->
+	<meta charset="utf-8">
+	<!-- set the viewport width and initial-scale on mobile devices -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- set the apple mobile web app capable -->
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<!-- set the HandheldFriendly -->
+	<meta name="HandheldFriendly" content="True">
+	<!-- set the apple mobile web app status bar style -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<!-- set the description -->
+	<meta name="description" content="Dekko - Construction HTML5 Template">
+	<!-- set the Keyword -->
+	<meta name="keywords" content="blog, clean, clear, creative, design web, ecommerce, flat, Indoor Furniture, marketing, portfolio, vineyard, wines, winewinery">
+	<meta name="author" content="Dekko - Construction HTML5 Template">
+	<title>Dekko - Construction HTML5 Template</title>
+	<!-- include the site stylesheet -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600" rel="stylesheet">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/style-construction.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/plugins.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/responsive-construction.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/iconfonts.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/color-construction.css">
+	<!-- Font awesome CSS -->
+	<link rel="stylesheet" href="css/all.css">
+	<!-- Google Font Thai : Mitr -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap" rel="stylesheet">
+	<style>
+		#thai {
+			font-family: 'Mitr', sans-serif;
+		}
+	</style>
+</head>
+
+<body>
+	<div id="wrapper">
+
+		<!-- NAV BAR -->
+		<?php include("include/navbar.html") ?>
+		<!-- NAV BAR END -->
+
+		<main id="main">
+			<!-- banner sec of the page -->
+			<section class="banner-sec">
+				<!-- slide of the page -->
+				<div class="slide bg-full overlay" style="background-image: url(images/background.png);">
+					<!-- content of the banner -->
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="caption text-center">
+									<span class="title text-uppercase">welcome to my</span>
+									<h1>Original Private Blog</h1>
+									<p>I'm just one of a normal <span style="color: #75FF00;">Computer Engineering</span> that love <span style="color: #00E2FF;">CODING</span> and be <span style="color: #FF0000;">GAMER</span> sometimes</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- col-timing of the page -->
+					<div class="col-timing">
+						<!-- list-timing of the page -->
+						<ul class="list-unstyled list-timing">
+							<li class="hours">BLOG HOURS</li>
+							<li><span>Mon - Sat :</span> 18:00 - 23:00</li>
+							<li><span>Sun :</span> CLOSE</li>
+						</ul>
+						<!-- clist-language of the page -->
+						<!-- <ul class="list-unstyled list-language">
+							<li><a href="javascript:void(0);">EN</a></li>
+							<li><a href="javascript:void(0);">FR</a></li>
+							<li><a href="javascript:void(0);">VN</a></li>
+						</ul> -->
+					</div>
+				</div>
+			</section>
+			<!-- abt sec of the page -->
+			<section class="abt-sec">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12">
+							<!-- content of the abt sec -->
+							<div class="header text-center">
+								<h2 class="heading2">
+									<span class="title text-uppercase">
+										Youtube Channel
+									</span>
+									Game & Programming
+								</h2>
+								<p>
+									<i class="fab fa-youtube"></i>
+									<b>Youtube :</b>
+									<a href="https://www.youtube.com/channel/UCNbcpa-ka7rH3H8c_L6grWw" style="color: #FF0000;">HALICE channel</a>
+								</p>
+							</div>
+						</div>
+						<div class="col-sm-12 text-center">
+							<!-- img-holder of the page -->
+							<div class="img-holder">
+								<!-- <img src="http://placehold.it/900x470" alt="Dekko-description" class="img-responsive"> -->
+								<iframe width="900" height="470" src="https://www.youtube.com/embed/2soNywkY9OA">
+								</iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- promo box of the page -->
+			<div class="promo-box">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-8 col-md-9">
+							<h2 class="heading3 text-center">
+								If You want to watch Call of Duty Mobile, then Click the button
+							</h2>
+						</div>
+						<div class="col-xs-12 col-sm-4 col-md-3 text-center">
+							<a href="https://www.facebook.com/halice00" target="_blank" style="color: #405ca1;" class="btn-primary bg-white text-uppercase">
+								<i class="fab fa-facebook-square" style="color: #405ca1;"></i>
+								Facebook Page
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- service sec of the page -->
+			<section class="service-sec">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="header text-center">
+								<h2 class="heading2"><span class="title text-uppercase">languages</span>What I Coding ?</h2>
+								<!-- <p>Which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need<br class="hidden-xs hidden-sm hidden-md">to be sure there isn't anything embarrassing hidden in the middle of text.</p> -->
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<!-- service col of the page -->
+							<div class="service-col text-center">
+								<span class="icon ti-desktop"></span>
+								<h2 class="heading1">Website create & design</h2>
+								<p>Language : HTML , CSS , PHP<br class="hidden-xs hidden-sm hidden-md">Framwork : Codeigniter<br class="hidden-xs hidden-sm hidden-md">Other : PHP CI MANIA , Bootstrap</p>
+								<div class="sc1">
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<!-- service col of the page -->
+							<div class="service-col text-center">
+								<span class="icon ti-image"></span>
+								<h2 class="heading1">Deep Learning</h2>
+								<p>Language : Python<br class="hidden-xs hidden-sm hidden-md">Library : Tensorflow , TFlite , Keras<br class="hidden-xs hidden-sm hidden-md">Other : OpenCV , Pillow</p>
+								<div class="sc1">
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<!-- service col of the page -->
+							<div class="service-col text-center">
+								<span class="icon ti-settings"></span>
+								<h2 class="heading1">Mini Robot Project</h2>
+								<p>Language : C/C# , Arduino<br class="hidden-xs hidden-sm hidden-md">Modules : Fire Detect Sensor ,<br class="hidden-xs hidden-sm hidden-md">LED Sensor , Bluetooth Sensor</p>
+								<div class="sc1">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- portfolio sec of the page -->
+			<section class="portfolio-sec">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="header text-center">
+								<h2 class="heading2 text-center"><span class="title text-uppercase">projects</span>Recent Our Projects</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- project-slider of the page -->
+				<div class="project-slider" id="thai">
+					<!-- slide of the page -->
+					<div class="portfolio-slider">
+						<div class="project-wrap">
+							<img src="images/mango.png" class="img-responsive" alt="Dekko-description">
+							<div class="portfolio-detail">
+								<h2 class="heading3">
+									<a href="https://github.com/MidnightCore/Image-Processing-by-OpenCV-for-Color-Detection">Image Processing by OpenCV <br>for Mango Color Detection</a>
+								</h2>
+								<p class="hidden-xs">รายวิชาการประมวลผลภาพ ภาคเรียนที่ 1 ปีการศึกษา 2563 ชิ้นงานนี้ได้จัดทำขึ้นด้วยการ<br class="hidden-xs">นำเทคโนโลยีการประมวลผลภาพมาใช้ในการตวรจจับสี เพื่อตวรจสอบและแยกสีของมะม่วงว่าสุกในระดับไหน<br class="hidden-xs">ประกอบไปด้วย ระดับมะม่วงดิบ ระดับมะม่วงสุกๆดิบๆ และระดับมะม่วงสุก</p>
+							</div>
+						</div>
+					</div>
+					<!-- slide of the page -->
+					<div class="portfolio-slider">
+						<div class="project-wrap">
+							<img src="images/car_rent.png" class="img-responsive" alt="Dekko-description">
+							<div class="portfolio-detail">
+								<h2 class="heading3"><a href="https://github.com/MidnightCore/Materialize_car_rent">Suan Sunandha Rajabhat University <br>General Education Car Reservation Service</a></h2>
+								<p class="hidden-xs">ระบบ Car Reservation Service สำหรับเจ้าหน้าที่และอาจารย์ประจำสำนักงานการศึกษาทั่วไป<br class="hidden-xs">มหาวิทยาลัยราชภัฏสวนสุนันทาเท่านั้น</p>
+							</div>
+						</div>
+					</div>
+					<!-- slide of the page -->
+					<div class="portfolio-slider">
+						<div class="project-wrap">
+							<img src="images/covid.png" class="img-responsive" alt="Dekko-description">
+							<div class="portfolio-detail">
+								<h2 class="heading3"><a href="https://github.com/MidnightCore/covid2020">SaveMed Covid donation Web Application<br>for fighting Covid19 </a></h2>
+								<p class="hidden-xs">Web Application สำหรับประชาสัมพันธ์การรับบริจาคหน้ากากอนามัย ให้กับทางโรงพยาบาลต่าง<br class="hidden-xs">สร้างขึ้นในนามของสาขาวิชาวิศวกรรมคอมพิวเตอร์ มหาวิทยาลัยราชภัฏสวนสุนันทา</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- choose sec of the page -->
+			<section class="choose-sec">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-6">
+							<!-- img-col of the page -->
+							<div class="img-col">
+								<img src="images/robot.png" alt="Dekko-description" class="img-responsive">
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-12 col-md-6">
+							<div class="header">
+								<h2 class="heading2"><span class="title text-uppercase">mini project</span>Flame Detection Robot</h2>
+								<p id="thai">หุ่นยนต์ประเภทล้อตะขาบบังคับมือ ควบคุมผ่านบลูทูธ สำหรับตรวจจับเปลวไฟ<br class="hidden-xs hidden-sm hidden-md">พร้อมหลอดไฟ LED แจ้งเตือนการตรวจจับ โดยเขียนลำดับการทำงานผ่านบอร์ด Arduino<br class="hidden-xs hidden-sm hidden-md"> และแปลงพลังงานจากแบบเตอร์รี่เข้าสู่บอร์ดผ่าน Motor Driver</p>
+							</div>
+							<ul class="list-unstyled choose-list">
+								<!-- choose list of the page -->
+								<li>
+									<span class="icon ti-medall"></span>
+									<div class="txt-holder">
+										<h3 class="heading1" id="thai">ประสิทธิภาพการทำงาน</h3>
+										<p>A talented professor who has published World Class<br class="hidden-xs hidden-sm hidden-md">Academic papers on IEEE website.</p>
+									</div>
+								</li>
+								<li>
+									<span class="icon ti-harddrives"></span>
+									<div class="txt-holder">
+										<h3 class="heading1" id="thai">อะไหล่และชิ้นส่วน</h3>
+										<p>Fully equipped Smart Classroom<br class="hidden-xs hidden-sm hidden-md"> with Apple IMAC Labroratory.</p>
+									</div>
+								</li>
+								<li>
+									<span class="icon ti-book"></span>
+									<div class="txt-holder">
+										<h3 class="heading1" id="thai">การทำงานตรงตามวัตถุประสงค์</h3>
+										<p>Don't be distracted by criticism. Remember the<br class="hidden-xs hidden-sm hidden-md"> only taste of success some people.</p>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- testimonail sec of the page -->
+			<section class="testimonail-sec bg-full" style="background-image: url(http://placehold.it/1920x750);">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="header text-center">
+								<h2 class="heading2"><span class="title text-uppercase">testimonials</span>From Clients Say</h2>
+							</div>
+							<!-- testimonail slider of the page -->
+							<div class="testimonail-slider">
+								<!-- slide of the page -->
+								<blockquote class="slide text-center">
+									<q>Your organization has performed professionally… Their commitment to quality and the extraordinary<br class="hidden-xs hidden-sm hidden-md"> efforts in ensuring the ultimate success of this project was wonderful. I wouldn’t hesitate to recommend<br class="hidden-xs hidden-sm hidden-md"> Henning for anything. I look forward to working with your organization again soon.</q>
+									<cite class="text-uppercase">Kawhi Leonard</cite>
+									<span>CEO DeerCreative Inc</span>
+								</blockquote>
+								<!-- slide of the page -->
+								<blockquote class="slide text-center">
+									<q>Your organization has performed professionally… Their commitment to quality and the extraordinary<br class="hidden-xs hidden-sm hidden-md"> efforts in ensuring the ultimate success of this project was wonderful. I wouldn’t hesitate to recommend<br class="hidden-xs hidden-sm hidden-md"> Henning for anything. I look forward to working with your organization again soon.</q>
+									<cite class="text-uppercase">Kawhi Leonard</cite>
+									<span>CEO DeerCreative Inc</span>
+								</blockquote>
+								<!-- slide of the page -->
+								<blockquote class="slide text-center">
+									<q>Your organization has performed professionally… Their commitment to quality and the extraordinary<br class="hidden-xs hidden-sm hidden-md"> efforts in ensuring the ultimate success of this project was wonderful. I wouldn’t hesitate to recommend<br class="hidden-xs hidden-sm hidden-md"> Henning for anything. I look forward to working with your organization again soon.</q>
+									<cite class="text-uppercase">Kawhi Leonard</cite>
+									<span>CEO DeerCreative Inc</span>
+								</blockquote>
+							</div>
+						</div>
+						<div class="col-xs-12">
+							<div class="partner-holder">
+								<!-- partner-holder of the page -->
+								<div class="partner-list">
+									<div class="partner-img">
+										<a href="javascript:void(0);" class="partner">
+											<img src="http://placehold.it/95x90" alt="oceandor" class="img-responsive">
+										</a>
+									</div>
+									<div class="partner-img">
+										<a href="javascript:void(0);" class="partner">
+											<img src="http://placehold.it/95x90" alt="design" class="img-responsive">
+										</a>
+									</div>
+									<div class="partner-img">
+										<a href="javascript:void(0);" class="partner">
+											<img src="http://placehold.it/95x90" alt="Dekko-partner" class="img-responsive">
+										</a>
+									</div>
+									<div class="partner-img">
+										<a href="javascript:void(0);" class="partner">
+											<img src="http://placehold.it/95x90" alt="retrodesign" class="img-responsive">
+										</a>
+									</div>
+									<div class="partner-img">
+										<a href="javascript:void(0);" class="partner">
+											<img src="http://placehold.it/95x90" alt="premium" class="img-responsive">
+										</a>
+									</div>
+									<div class="partner-img">
+										<a href="javascript:void(0);" class="partner">
+											<img src="http://placehold.it/95x90" alt="oceandor" class="img-responsive">
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- blog sec of the page -->
+			<section class="blog-sec">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12">
+							<h2 class="heading2 text-center"><span class="title text-uppercase">blogs</span>Ours Blog Post</h2>
+						</div>
+						<div class="col-xs-6 col-sm-6 col-md-4 sm2">
+							<!-- blog-col of the page -->
+							<div class="blog-col">
+								<img src="http://placehold.it/370x370" alt="Dekko-description" class="img-responsive blog-img">
+								<!-- blog-detail of the page -->
+								<div class="blog-detail text-center">
+									<h2 class="heading1">Berlin Rooftop Workshop</h2>
+									<ul class="list-unstyled post-list">
+										<li><a href="blog-detail.php">By admin</a></li>
+										<li>|</li>
+										<li><time class="time" datetime="2017-01-01">Dec 24, 2018</time></li>
+									</ul>
+									<p>There are many variations of passages of<br class="hidden-xs hidden-sm hidden-md"> Lorem Ipsum available, but the majority<br class="hidden-xs hidden-sm hidden-md"> have suffered alteration in some form</p>
+									<a class="blog-link text-uppercase" href="blog-detail3.php">read more</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-6 col-sm-6 col-md-4 sm2">
+							<!-- blog-col of the page -->
+							<div class="blog-col">
+								<img src="http://placehold.it/370x370" alt="Dekko-description" class="img-responsive blog-img">
+								<!-- blog-detail of the page -->
+								<div class="blog-detail text-center">
+									<h2 class="heading1">Berlin Rooftop Workshop</h2>
+									<ul class="list-unstyled post-list">
+										<li><a href="blog-detail.php">By admin</a></li>
+										<li>|</li>
+										<li><time class="time" datetime="2017-01-01">Dec 24, 2018</time></li>
+									</ul>
+									<p>There are many variations of passages of<br class="hidden-xs hidden-sm hidden-md"> Lorem Ipsum available, but the majority<br class="hidden-xs hidden-sm hidden-md"> have suffered alteration in some form</p>
+									<a class="blog-link text-uppercase" href="blog-detail3.php">read more</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-6 col-sm-6 col-md-4 sm2">
+							<!-- blog-col of the page -->
+							<div class="blog-col">
+								<img src="http://placehold.it/370x370" alt="Dekko-description" class="img-responsive blog-img">
+								<!-- blog-detail of the page -->
+								<div class="blog-detail text-center">
+									<h2 class="heading1">Berlin Rooftop Workshop</h2>
+									<ul class="list-unstyled post-list">
+										<li><a href="blog-detail.php">By admin</a></li>
+										<li>|</li>
+										<li><time class="time" datetime="2017-01-01">Dec 24, 2018</time></li>
+									</ul>
+									<p>There are many variations of passages of<br class="hidden-xs hidden-sm hidden-md"> Lorem Ipsum available, but the majority<br class="hidden-xs hidden-sm hidden-md"> have suffered alteration in some form</p>
+									<a class="blog-link text-uppercase" href="blog-detail3.php">read more</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+		
+		<?php include("include/footer.html") ?>
+
+		<!--back-top of the page -->
+		<a id="back-top" class="text-center btn-primary fa fa-angle-up"></a>
+	</div>
+	<!-- include jQuery -->
+	<script src="js/jquery.js"></script>
+	<!-- include jQuery -->
+	<script src="js/plugins.js"></script>
+	<!-- include jQuery -->
+	<script src="js/jquery.main.js"></script>
+</body>
+
+</html>
